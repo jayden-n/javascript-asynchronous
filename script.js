e2cfb8fbde8f60caee1210489f86a2123209342f
jayden-n <jaydennguyen.dev@gmail.com>
2023-02-07 13:40:45 -0500

    chaining promises

--- script.js
+++ script.js
@@ -70,13 +70,21 @@ getCountryAndNeighbour('usa');
 const request = fetch('https://restcountries.com/v2/name/vietnam');
 
 const getCountryData = country => {
+  // Country 1
   fetch(`https://restcountries.com/v2/name/${country}`)
     .then(response => {
       return response.json();
     })
     .then(data => {
-      console.log(data);
       renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+      // Country 2
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`)
+        .then(response => response.json())
+        .then(data => {
+          renderCountry(data, 'neighbour');
+        });
     });
 };
-getCountryData('vietnam');
+getCountryData('usa');
